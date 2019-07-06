ffcdf16ec29487e869038ff8c6d46b507fb48a5c
Istvan Nyolczas <31048051+Nyolczas@users.noreply.github.com>
2019-07-06 20:53:11 +0200

    contact form

--- index.php
+++ index.php
@@ -7,27 +7,29 @@ $title = 'Nyolczas István';
 include 'includes\head.php';
 ?>
 
-<body>
+<body class="index-body d-flex flex-column align-items-stretch">
 
     <?php include 'includes\navbar.php'; ?>
 
-    <div class="container">
-        <div class="message">
-            <div class="message-img"></div>
-            <div class="message-txt">
-                <h6> Helló, a nevem <span class="text-warning">Nyolczas István</span>, vizuális szakember vagyok.</h6>
-                <h1>Miben segíthetek?</h1>
+    <div class="midlerow">
+        <div class="container">
+            <div class="message">
+                <div class="message-img"></div>
+                <div class="message-txt">
+                    <h6> Helló, a nevem <span class="text-warning">Nyolczas István</span>, vizuális szakember vagyok.</h6>
+                    <h1>Miben segíthetek?</h1>
+                </div>
             </div>
         </div>
+        <?php include 'includes\carousel.php'; ?>
     </div>
 
-    <?php include 'includes\carousel.php'; ?>
-
     <div class="kapcsolat-box w-100">
-        <div class="container d-flex justify-content-end align-items-center w-100 h-100">
+        <div class="container d-flex justify-content-center align-items-center w-100 h-100">
             <a href="kapcsolat.php" class="btn btn-outline-warning">Kapcsolat</a>
         </div>
     </div>
+    
 
 </body>
 </html>
--- kapcsolat.php
+++ kapcsolat.php
@@ -13,6 +13,34 @@ include 'includes\head.php';
 include 'includes\navbar.php';
 
 ?>
+<div class="container d-flex justify-content-center">
+    <form class="contact-form">
+      <fieldset>
+        <legend>Küldj egy üzenetet!</legend>
+        <div class="form-group row">
+        </div>
+
+        <div class="form-group">
+          <label for="inputName">Neved:</label>
+          <input type="text" class="form-control" id="inputName" placeholder="Nevem">
+        </div>
+
+        <div class="form-group">
+          <label for="inputEmail">Email címed:</label>
+          <input type="email" class="form-control" id="inputEmail" aria-describedby="emailHelp" placeholder="Email címem">
+          <small id="emailHelp" class="form-text text-muted">Neved és email címed senkinek nem adom tovább.</small>
+        </div>
+        
+       
+        <div class="form-group">
+          <label for="inputMessage">Üzeneted:</label>
+          <textarea class="form-control" id="inputMessage" rows="3" style="margin-top: 0px; margin-bottom: 0px; height: 101px;"></textarea>
+        </div>
+       
+        <button type="submit" class="btn btn-primary btn-lg btn-block">Elküldöm</button>
+      </fieldset>
+    </form>
+</div>
 
 </body>
 </html>
