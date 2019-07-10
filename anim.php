b689516e7581abbf4d9c68e5bb5fb2ece1cc1df9
Nyolczas <nyolczas@gmail.com>
2019-07-10 22:53:23 +0200

    anim

--- anim.php
+++ anim.php
@@ -1,63 +1,38 @@
 <?php
-$pageNr = 2;
 include 'includes\menuArray.php';
+include 'includes\animArray.php';
+include 'includes\modalBuilder.php';
+$pageNr = 2;
 $page = $menuArray[$pageNr]['pageName'];
 $title = $menuArray[$pageNr]['bjutiName'] . ' | Nyolczas István';
-$animVideos = [
-    [
-        "name" => "Showreel 2014",
-        "desc" => "showreel",
-        "link" => "https://www.youtube.com/embed/3HcETldc8ZE"
-    ],[
-        "name" => "Projection mapping reel",
-        "desc" => "showreel",
-        "link" => "https://www.youtube.com/embed/Tcey9BVfn7o"
-    ],[
-        "name" => "Olimpos Ice Tea TV spot",
-        "desc" => "2D animáció",
-        "link" => "https://www.youtube.com/embed/aYXhNiaWIfQ"
-    ],[
-        "name" => "Maggi Kockák TV spot",
-        "desc" => "2D animáció, vfx",
-        "link" => "https://www.youtube.com/embed/EDYWGerZ7WM"
-    ],[
-        "name" => "Westend Karácsony TV spot",
-        "desc" => "3D general, vfx",
-        "link" => "https://www.youtube.com/embed/YuOEB5fNETo"
-    ],[
-        "name" => "Magyar Tej TV spot",
-        "desc" => "concept, 3D general",
-        "link" => "https://www.youtube.com/embed/mNuJuKeMpsA"
-    ],[
-        "name" => "Mama",
-        "desc" => "concept, art design, 3D general",
-        "link" => "https://www.youtube.com/embed/oItCsGOCs24"
-    ],[
-        "name" => "CinemaCity Alle",
-        "desc" => "concept, art design, 2D animáció, 3D general",
-        "link" => "https://www.youtube.com/embed/CmAgBvX2KTo"
-    ],[
-        "name" => "Showreel 2007",
-        "desc" => "showreel",
-        "link" => "https://www.youtube.com/embed/WUBKIFR_FwM"
-    ]    
-];
+
 include 'includes\head.php';
 ?>
 
 <body>
 <?php include 'includes\contentHeader.php'; ?>
-<div class="container">
-    <div class="row">
+<div class="container my-4">
+    <div class="row my-4">
         <div class="col-sm-8">
             <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
             <p>It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
         </div>
-        <div class="col-sm-4">
-            <a href="kapcsolat.php" class="btn btn-outline-warning">Kapcsolat</a>
+        <div class="d-flex col-sm-4 align-self-end justify-content-end mb-4">
+            <a href="kapcsolat.php" class="btn btn-warning">Kapcsolat</a>
         </div>
     </div>
+    <h3 class="mb-4">Animációs referenciák</h3>
+    <div class="row mb-4">
+        <!-- Button trigger modals -->
+        <?php
+            foreach ($animVideos as $anim) {
+                modalButton(3, $anim['name'], 'anim', $anim['titl']);
+            }
+        ?> 
+    </div>
     <?php
+
+    /*
         $cnt = 0;
         foreach ($animVideos as $video){
             if($cnt == 0) {echo '<div class="row">';}
@@ -75,6 +50,7 @@ include 'includes\head.php';
             $cnt++;
             if($cnt == 3) {echo '</div>'; $cnt = 0;}
         }
+        */
     ?>
 </div>
 
---
+++ includes/animArray.php
@@ -0,0 +1,60 @@
+<?php
+$animVideos = [
+    [
+        "name" => "reel14",
+        "titl" => "Showreel 2014",
+        "desc" => "showreel",
+        "link" => "https://www.youtube.com/embed/3HcETldc8ZE"
+    ],[
+        "name" => "reel16",
+        "titl" => "Projection mapping reel",
+        "desc" => "showreel",
+        "link" => "https://www.youtube.com/embed/Tcey9BVfn7o"
+    ],[
+        "name" => "iceTea",
+        "titl" => "Olimpos Ice Tea TV spot",
+        "desc" => "2D animáció",
+        "link" => "https://www.youtube.com/embed/aYXhNiaWIfQ"
+    ],[
+        "name" => "maggiK",
+        "titl" => "Maggi Kockák TV spot",
+        "desc" => "2D animáció, vfx",
+        "link" => "https://www.youtube.com/embed/EDYWGerZ7WM"
+    ],[
+        "name" => "westend",
+        "titl" => "Westend Karácsony TV spot",
+        "desc" => "3D general, vfx",
+        "link" => "https://www.youtube.com/embed/YuOEB5fNETo"
+    ],[
+        "name" => "tej",
+        "titl" => "Magyar Tej TV spot",
+        "desc" => "concept, 3D general",
+        "link" => "https://www.youtube.com/embed/mNuJuKeMpsA"
+    ],[
+        "name" => "mama",
+        "titl" => "Mama",
+        "desc" => "concept, art design, 3D general",
+        "link" => "https://www.youtube.com/embed/oItCsGOCs24"
+    ],[
+        "name" => "alle",
+        "titl" => "CinemaCity Alle",
+        "desc" => "concept, art design, 2D animáció, 3D general",
+        "link" => "https://www.youtube.com/embed/CmAgBvX2KTo"
+    ],[
+        "name" => "reel7",
+        "titl" => "Showreel 2007",
+        "desc" => "showreel",
+        "link" => "https://www.youtube.com/embed/WUBKIFR_FwM"
+    ],[
+        "name" => "maggiS",
+        "titl" => "Maggi Gombócok",
+        "desc" => "Generál 3D",
+        "link" => "https://www.youtube.com/watch?v=PElm5C7CD9I"
+    ],[
+        "name" => "pain",
+        "titl" => "Fájdalom és Humorérzék",
+        "desc" => "Rutkai Borival",
+        "link" => "https://www.youtube.com/watch?v=bu5MHq16tVM"
+    ]    
+];
+?>
