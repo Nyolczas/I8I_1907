b070ee0b73e1d0e4d67fcfabdbd1759d17940d0d
Nyolczas <nyolczas@gmail.com>
2019-07-11 18:17:38 +0200

    illu

---
+++ includes/dddArray.php
@@ -0,0 +1,19 @@
+<?php
+$dddVideos = [
+    [
+        "name" => "reel14",
+        "titl" => "Showreel 2014",
+        "desc" => "showreel",
+        "link" => "https://www.youtube.com/embed/3HcETldc8ZE",
+    ],[
+        "name" => "maggiS",
+        "titl" => "Maggi Gombócok",
+        "desc" => "Generál 3D",
+        "link" => "https://www.youtube.com/embed/PElm5C7CD9I"
+    ],[
+        "name" => "mama",
+        "titl" => "Mama",
+        "desc" => "concept, art design, 3D general",
+        "link" => "https://www.youtube.com/embed/oItCsGOCs24"
+    ],
+];
