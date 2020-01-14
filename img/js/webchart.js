f39ef8b120ce157f08818a094ad5e9ae389e8270
Nyolczas <nyolczas@gmail.com>
2020-01-14 15:40:24 +0100

    referenciák: update, kompetenciák chart: start

---
+++ img/js/webchart.js
@@ -0,0 +1,17 @@
+
+var options = {
+  chart: {
+    type: 'bar'
+  },
+  series: [{
+    name: 'gyakorlat',
+    data: [100,100,96,98,97,99,97,75,49,60,55,33,60,40,73,57,29,23,18,19,25,9]
+  }],
+  xaxis: {
+    categories: ['HTML5','CSS3','SASS','Bootstrap','Javascript','ES6','Web animáció','JQuery','Node.js','NPM','React', 'Vue','Typescript','Webpack','PHP','Laravel','MySQL','Python','C#','ASP.NET','Visual Basic','Java']
+  }
+}
+
+var chart = new ApexCharts(document.querySelector("#chart"), options);
+
+chart.render();
--- includes/webArray.php
+++ includes/webArray.php
@@ -1,23 +1,116 @@
 <?php
 $webArr = [
     [
+        'name' => ['sport4life'],
+        'isImage' => false, 
+        'path' => 'http://sport4life.hu/',
+        'title' => 'Sport 4 Life weboldal',
+        'desc' => 'webdesign, logóterv, sitebuild, frontend fejlesztés'
+    ],
+    [
+        'name' => ['optowit'],
+        'isImage' => false, 
+        'path' => 'https://optowit.hu/',
+        'title' => 'Optowit webáruház',
+        'desc' => 'sitebuild, frontend fejlesztés'
+    ],
+    [
+        'name' => ['hr-evolution'],
+        'isImage' => false, 
+        'path' => 'https://hrevolution.hu/munkavallalo-tamogato-program/',
+        'title' => 'HR Evolution',
+        'desc' => 'sitebuild, frontend fejlesztés'
+    ],
+    [
+        'name' => ['helloMastercard'],
+        'isImage' => false, 
+        'path' => 'helloMastercard/',
+        'title' => 'Hello Mastercard',
+        'desc' => 'webdesign, sitebuild, frontend fejlesztés, 2D és 3D animációk'
+    ],
+    [
+        'name' => ['mogacs'],
+        'isImage' => false, 
+        'path' => 'https://mogacs.hu/',
+        'title' => 'Mogács Dániel weboldala',
+        'desc' => 'sitebuild, frontend fejlesztés, 2D animáció'
+    ],
+    [
+      'name' => ['tigaz'],
+      'isImage' => true,
+      'path' => 'web/tigaz',
+      'images' => [
+          [
+              'name' => 'tigaz_01',
+              'width' => '1500',
+          ],
+          [
+              'name' => 'tigaz_02',
+              'width' => '1500',
+          ],
+          [
+              'name' => 'tigaz_03',
+              'width' => '1500',
+          ],
+          [
+              'name' => 'tigaz_04',
+              'width' => '1500',
+          ],
+          [
+              'name' => 'tigaz_05',
+              'width' => '1500',
+          ],
+          [
+              'name' => 'tigaz_06',
+              'width' => '1440',
+          ],
+          [
+              'name' => 'tigaz_07',
+              'width' => '1440',
+          ],
+          [
+              'name' => 'tigaz_08',
+              'width' => '1440',
+          ],
+          [
+              'name' => 'tigaz_09',
+              'width' => '1440',
+          ],
+          [
+              'name' => 'tigaz_10',
+              'width' => '1440',
+          ],
+      ],
+      'title' => 'Tigáz weboldal',
+      'desc' => 'webdesign'
+  ],
+    [
         'name' => ['novopayment'],
         'isImage' => false, 
         'path' => 'https://novopayment.hu/',
         'title' => 'Novopayment.hu',
-        'desc' => 'webdesign, sitebuild, frontend fejlesztés'
-    ],[
+        'desc' => 'webdesign, sitebuild, frontend fejlesztés, interaktív animációk'
+    ],
+    [
         'name' => ['mabisz'],
         'isImage' => false, 
         'path' => 'https://jovotervezes.mabisz.hu/',
         'title' => 'Mabisz Jövőtervezés',
-        'desc' => 'sitebuild, frontend fejlesztés'
-    ],[
+        'desc' => 'sitebuild, frontend fejlesztés, 2D animációk'
+    ],
+    [
         'name' => ['e-sports'],
         'isImage' => false, 
         'path' => 'https://e-sports.hu/',
         'title' => 'e-sports.hu',
         'desc' => 'sitebuild, frontend fejlesztés'
+    ],
+    [
+        'name' => ['sajtostejfolos'],
+        'isImage' => false, 
+        'path' => 'http://nyolczas.hu/sajtostejfolos/',
+        'title' => 'Sajtos Tejfölös Büfé - fiktív weboldal',
+        'desc' => 'logóterv, webdesign, sitebuild, frontend fejlesztés'
     ],
     [
         'name' => ['bijo'],
@@ -63,61 +156,71 @@ $webArr = [
         ],
         'title' => 'Bijo webáruház',
         'desc' => 'webdesign'
-    ],[
+    ],
+    [
         'name' => ['ue'],
         'isImage' => false, 
         'path' => 'http://utasellatokavehaz.hu/',
         'title' => 'Utasellátó Kávéház',
         'desc' => 'Weboldal - webdesign és sitebuild'
-    ],[
+    ],
+    [
         'name' => ['rap'],
         'isImage' => false, 
         'path' => 'https://www.rapcity.hu/',
         'title' => 'RapCity',
         'desc' => 'Weboldal karbantartás'
-    ],[
+    ],
+    [
         'name' => ['grube'],
         'isImage' => false, 
         'path' => 'https://www.grube.hu/',
         'title' => 'Grube',
         'desc' => 'Weboldal karbantartás - grafikai munkák'
-    ],[
+    ],
+    [
         'name' => ['jkh'],
         'isImage' => false, 
         'path' => 'https://shop.jkh.hu/',
         'title' => 'JKH',
         'desc' => 'Weboldal karbantartás'
-    ],[
+    ],
+    [
         'name' => ['vectra'],
         'isImage' => false, 
         'path' => 'https://vectraline.hu/hu',
         'title' => 'Vectraline',
         'desc' => 'Weboldal karbantartás'
-    ],[
+    ],
+    [
         'name' => ['hellopinky'],
         'isImage' => false, 
         'path' => 'http://hellopinky.hu/',
         'title' => 'Hello Pinky',
         'desc' => 'Weboldal karbantartás'
-    ],[
+    ],
+    [
         'name' => ['priceCalc'],
         'isImage' => false, 
         'path' => 'PriceCalculator\price-calculator.html',
         'title' => 'Freelusion Price Calculator',
         'desc' => 'webfejlesztés - sitebuild'
-    ],[
+    ],
+    [
         'name' => ['priceList'],
         'isImage' => false, 
         'path' => 'Pricelist01\premium-dance-shows.html',
         'title' => 'Freelusion Price List',
         'desc' => 'Árlista katalógus - webdesign és sitebuild'
-    ],[
+    ],
+    [
         'name' => ['budget'],
         'isImage' => false, 
         'path' => 'myBudgety\budget.html',
         'title' => 'Költségvetés számoló',
         'desc' => 'Javascript - Havi költségvetés számoló'
-    ],[
+    ],
+    [
         'name' => ['sugi'],
         'isImage' => true,
         'path' => 'web/sugi',
@@ -169,7 +272,8 @@ $webArr = [
         ],
         'title' => 'Sugar fondant',
         'desc' => 'webdesign'
-    ],[
+    ],
+    [
         'name' => ['pig'],
         'isImage' => false, 
         'path' => 'pigGame\pigGame.html',
